9a1c1bf05eb9363cdbc27e45bb6e9f60871070ff
mdyer2 <132947537+mdyer2@users.noreply.github.com>
2024-07-29 11:46:48 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from ApplicationFactory import create_app
+from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-# Create the Flask application using the factory function
-app = create_app()
+app = Flask(__name__)
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(host='127.0.0.1', port=8000)
+
